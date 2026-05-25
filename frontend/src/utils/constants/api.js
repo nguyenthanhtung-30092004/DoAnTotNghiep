8f7f839d19309015341d4d1274dbceca157f2905
nguyenthanhtung-30092004 <tungnt2k4yyy@gmail.com>
2026-05-26 01:15:24 +0700

    Fix frontend API endpoints for production

--- frontend/src/utils/constants/api.js
+++ frontend/src/utils/constants/api.js
@@ -1,5 +1,5 @@
 export const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:3000/v1/api";
-export const API_AUTH = `${API_BASE}/user`;
+export const API_AUTH = `${API_BASE}/users`;
 export const API_REGISTER = `${API_AUTH}/register`;
 export const API_LOGIN = `${API_AUTH}/login`;
 export const API_LOGOUT = `${API_AUTH}/logout`;
