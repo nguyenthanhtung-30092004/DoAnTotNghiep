7be9593746a99f9636ac74aa2a8ca602b414b953
nguyenthanhtung-30092004 <tungnt2k4yyy@gmail.com>
2026-06-01 13:13:26 +0700

    fix order service import path

--- backend/src/modules/orders/order.controller.js
+++ backend/src/modules/orders/order.controller.js
@@ -1,5 +1,5 @@
 const { Created, OK } = require("../../core/success.response");
-const orderService = require("../../services/order.service");
+const orderService = require("../services/order.service");
 
 class OrderController {
   // user create order
@@ -89,4 +89,3 @@ class OrderController {
 }
 
 module.exports = new OrderController();
-
